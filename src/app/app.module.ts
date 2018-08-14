15366bf208e42202135202ff0ac27216b8e28b18
geisonmsilva12 <geisonmsilva12@gmail.com>
2018-08-14 00:30:10 -0300

    tela de chat criada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ChatPage } from '../pages/chat/chat-page';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ChatPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    ChatPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ChatPage } from '../chat/chat-page';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewCanLeave(){
+    
+  }
+
+  public abrirChat(){
+    this.navCtrl.push(ChatPage);
   }
 
 }
